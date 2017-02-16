8bd839ef57d4f6b94634c2f0d093d1a79dae4e3a
TJ Holowaychuk <tj@vision-media.ca>
2017-02-16 09:32:18 -0800

    normalize a bit more

--- gopherjs.mk
+++ gopherjs.mk
@@ -1,18 +1,18 @@
 GOPHERJS_PKG=$(subst $(GOPATH)/src/,,$(PWD))
 
-# Start the dev server.
-start:
+# Start client dev server.
+client.start:
 	@gopherjs -m -v serve --http :3000 $(GOPHERJS_PKG)/client
-.PHONY: start
+.PHONY: client.start
 
-# Display dependency graph.
-deps.graph:
+# Output client dependency graph.
+client.deps.graph:
 	@godepgraph $(GOPHERJS_PKG)/client | dot -Tsvg | browser
-.PHONY: deps.graph
+.PHONY: client.deps.graph
 
-# Display size of dependencies.
-deps.size:
+# Output client dependency sizes.
+client.deps.size:
 	@gopherjs build client/*.go -m -o /tmp/out.js
 	@du -h /tmp/out.js
 	@gopher-count /tmp/out.js | sort -nr
-.PHONY: deps.size
+.PHONY: client.deps.size
