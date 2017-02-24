532808f69a5dd8fd1b7011715edd5922107d862f
TJ Holowaychuk <tj@vision-media.ca>
2017-02-24 11:18:36 -0800

    change static start to be recursive

--- static.mk
+++ static.mk
@@ -3,5 +3,5 @@ STATIC_DIR ?= public
 
 # Start static file server.
 start:
-	@browser-sync start --server --serveStatic public --files "$(STATIC_DIR)/*"
+	@browser-sync start --server --serveStatic public --files "$(STATIC_DIR)/**"
 .PHONY: start
