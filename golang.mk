ebd95b16e59ad304d0a4e40cfeeee00f377ec12f
TJ Holowaychuk <tj@vision-media.ca>
2017-02-28 09:19:50 -0800

    add golang test, install, and build targets

--- golang.mk
+++ golang.mk
@@ -1,2 +1,17 @@
 include github.com/tj/make/stats
 include github.com/tj/make/todo
+
+# Run all tests.
+test:
+	@go test -cover ./...
+.PHONY: test
+
+# Install the commands.
+install:
+	@go install ./cmd/...
+.PHONY: install
+
+# Build release binaries.
+build:
+	@gox -os="linux darwin windows openbsd" ./...
+.PHONY: build
