c357de90fcc77f8369e1fcfd9df1652b31f04be8
TJ Holowaychuk <tj@vision-media.ca>
2017-03-01 15:35:20 -0800

    rename to S3_SYNC_DIR

--- static/s3.mk
+++ static/s3.mk
@@ -3,12 +3,12 @@ ifndef S3_BUCKET
 $(error S3_BUCKET is required)
 endif
 
-ifndef S3_DIR
-$(error S3_DIR is required, and should point to the static files directory for syncing)
+ifndef S3_SYNC_DIR
+$(error S3_SYNC_DIR is required, and should point to the static files directory for syncing)
 endif
 
 # Sync static site to S3.
 s3.sync:
-	@echo "==> syncing $(S3_DIR) to $(S3_BUCKET)"
-	@aws s3 sync $(S3_DIR) s3://$(S3_BUCKET)
+	@echo "==> syncing $(S3_SYNC_DIR) to $(S3_BUCKET)"
+	@aws s3 sync $(S3_SYNC_DIR) s3://$(S3_BUCKET)
 .PHONY: static.sync
