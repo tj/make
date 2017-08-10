ed30bb690fe0f86f62f013af6b0e3027ce3a5e40
TJ Holowaychuk <tj@vision-media.ca>
2017-08-09 18:59:50 -0700

    add golang size target

--- golang.mk
+++ golang.mk
@@ -15,3 +15,8 @@ install:
 release:
 	@goreleaser --rm-dist --config .goreleaser.yml
 .PHONY: release
+
+# Show size of imports.
+size:
+	@curl -sL https://gist.githubusercontent.com/tj/04e0965e23da00ca33f101e5b2ed4ed4/raw/9aa16698b2bc606cf911219ea540972edef05c4b/gistfile1.txt | bash
+.PHONY: size
